694bcb5be39f065ba27cef835aca6ecb72c465c4
Carlos Emanuel <devcarlos0112@gmail.com>
2025-04-06 10:32:24 -0300

    Resetando Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import Container from './components/layout/Container';
-import Header from './pages/Header';
-import Tasks from './pages/Tasks';
-import Task from '../src/pages/Task'
 
 function App() {
   return (
-    <Router>
-      
-
-      <Container>
-      <Header />
-
-      <Tasks />
-        <Routes>
-            <Route path="/tasks/:id" element={<Task />}></Route>
-        </Routes>
-      </Container>  
-    </Router>
-           
+    <h1>Projeto</h1>
   );
 }
 
